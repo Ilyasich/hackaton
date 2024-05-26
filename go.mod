04c2a998ab29a65f98d989b1a7c7005d4a03e4e9
ConstantineMikhalev <mikhalevk@gmail.com>
2024-05-26 14:39:41 +0400

    check balance

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module hackaton
 
 go 1.21.1
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+require github.com/go-resty/resty/v2 v2.13.1
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	golang.org/x/net v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	//"Testbot/services"
 )
@@ -22,17 +27,64 @@ func main() {
 		if update.Message.Chat.Type != "private" {
 			continue
 		}
-		//response := make(map[string]interface{})
+		
+		if checkBalance() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "https://t.me/+8EjQh3YfAyc4Yjky") //fmt.Sprint(response["balance"].(float64)))
+			msg.ReplyToMessageID = update.Message.MessageID
 
-		//acc, _ := client.R().Get("https://tonapi.io/v2/accounts/UQCPZiICYEhhTC0xdYXLtpVKK4LBsDSJmZFl6ilJEP0oVR7y")
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
+		}	else {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "no group for you") //fmt.Sprint(response["balance"].(float64)))
+			msg.ReplyToMessageID = update.Message.MessageID
 
-		//json.Unmarshal(acc.Body(), &response)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "https://t.me/+8EjQh3YfAyc4Yjky") //fmt.Sprint(response["balance"].(float64)))
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		if _, err := bot.Send(msg); err != nil {
-			panic(err)
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
 		}
+	}
+}
+
+func checkBalance() bool {
+	req, err := http.NewRequest("GET", "https://tonapi.io/v2/accounts/UQCPZiICYEhhTC0xdYXLtpVKK4LBsDSJmZFl6ilJEP0oVR7y", nil)
+	fmt.Println(req, err)
+	response := make(map[string]interface{})
+
+	// Define the client
+	client := resty.New()
+	acc, _ := client.R().Get("https://tonapi.io/v2/accounts/UQCPZiICYEhhTC0xdYXLtpVKK4LBsDSJmZFl6ilJEP0oVR7y")
+	json.Unmarshal(acc.Body(), &response)
+
+	fmt.Println(json.Unmarshal(acc.Body(), &response))
+
+	// Check if data parsing was successful (optional)
+	if response == nil {
+		fmt.Println("Error parsing response")
+		// Handle error
+		return (false)
+	}
+
+	// Access the balance
+	balance, ok := response["balance"]
+
+	if !ok {
+		fmt.Println("Balance key not found in response")
+		// Handle missing key
+		return (false)
+	}
+
+	if balanceInt, ok := balance.(float64); ok {
+		if balanceInt > 0 {
+			fmt.Println("go to our group")
+			return true
+		} else {
+			fmt.Println("no group")
+			return false
+		}
+	} else {
+		fmt.Println("Error converting balance to integer")
+		// Handle conversion error
+		return false
 	}
 }
