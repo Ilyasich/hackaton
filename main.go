5ea11f9c6a3456a508ff1c4ccbd6e20dfb5e5bcb
ConstantineMikhalev <mikhalevk@gmail.com>
2024-05-26 13:12:33 +0400

    go bot sends message

--- main.go
+++ main.go
@@ -1,2 +1,38 @@
 package main
 
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	//"Testbot/services"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("7122538965:AAGKBU7LncihcNWszcx5Pw64N9_be4U4zSs")
+	if err != nil {
+		panic(err)
+	}
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 30
+	updates := bot.GetUpdatesChan(updateConfig)
+	//client := resty.New()
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.Chat.Type != "private" {
+			continue
+		}
+		//response := make(map[string]interface{})
+
+		//acc, _ := client.R().Get("https://tonapi.io/v2/accounts/UQCPZiICYEhhTC0xdYXLtpVKK4LBsDSJmZFl6ilJEP0oVR7y")
+
+		//json.Unmarshal(acc.Body(), &response)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "https://t.me/+8EjQh3YfAyc4Yjky") //fmt.Sprint(response["balance"].(float64)))
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+	}
+}
